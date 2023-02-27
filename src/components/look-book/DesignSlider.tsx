9b8e03cfec03f228dbca88da0cad15a02f43e911
DarkSky3309 <lust3309@gmail.com>
2023-02-27 18:27:50 +0300

    slider done

--- src/components/look-book/DesignSlider.tsx
+++ src/components/look-book/DesignSlider.tsx
@@ -1,7 +1,12 @@
 import React, {FC, MutableRefObject, useEffect, useRef, useState} from 'react';
 import {motion} from "framer-motion";
 import images from "./images";
-const DesignSlider = () => {
+
+interface SliderProps{
+    slides: {term: string, area: string, price: string}[]
+}
+
+const DesignSlider:FC<SliderProps> = ({slides}) => {
     const [width, setWidth] = useState(0);
     const carousel = useRef() as React.MutableRefObject<HTMLDivElement>
 
@@ -12,9 +17,14 @@ const DesignSlider = () => {
         <div className={"slider"}>
             <motion.div ref={carousel} className={"carousel"} whileTap={{cursor: "grabbing"}}>
                 <motion.div drag={"x"} dragConstraints={{right:0, left: -width}} className={"carousel__inner"}>
-                    {images.map(image => {
+                    {images.map((image, index) => {
                         return(
-                            <motion.div key={image} className={"slide"}><img src={image} alt={"design"}/></motion.div>
+                            <motion.div key={image} className={"slide"}>
+                                <img src={image} alt={"design"}/>
+                                <div className={"slide-info"}>{slides[index].term}</div>
+                                <div className={"slide-info"}>{slides[index].area}</div>
+                                <div className={"slide-info"}>{slides[index].price}</div>
+                            </motion.div>
                         )})
                     }
                 </motion.div>
--- src/components/look-book/Lookbook.tsx
+++ src/components/look-book/Lookbook.tsx
@@ -3,11 +3,41 @@ import "./../../scss/lookbook.css"
 import DesignSlider from "./DesignSlider";
 
 const Lookbook = () => {
-
+    const slides = [
+        {
+            term: "Desig. in 1 m.",
+            area: "56 m2",
+            price: "1 m2 - 17$"
+        },
+        {
+            term: "Desig. in 3 weeks",
+            area: "32 m2",
+            price: "1 m2 - 19$"
+        },
+        {
+            term: "Desig. in 2 weeks",
+            area: "27 m2",
+            price: "1 m2 - 15$"
+        },
+        {
+            term: "Desig. in 6 weeks",
+            area: "98 m2",
+            price: "1 m2 - 20$"
+        },
+        {
+            term: "Desig. in 5 weeks",
+            area: "48 m2",
+            price: "1 m2 - 17$"
+        },
+        {
+            term: "Desig. in 2 weeks",
+            area: "25 m2",
+            price: "1 m2 - 20$"
+        }]
     return (
         <div className={"lookBook"}>
             <div className={"slider__wrapper"}>
-                <DesignSlider/>
+                <DesignSlider slides={slides}/>
             </div>
         </div>
     );
